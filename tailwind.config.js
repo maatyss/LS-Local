a38177840916ed5ace3ad3ea22cdd6d0d3db592d
Maatyss <mvancaemelbecke@gmail.com>
2025-03-07 06:25:38 -0500

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,25 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
     extend: {
       fontFamily: {
         lato: ['Lato', 'serif'],
         nunito: ['Nunito', 'sans-serif'],
+      },
+      colors: {
+        lavender: '#FCEFEF',
+        silver: '#BDBDBD',
+        electric: {
+          blue: '#0D47A1',
+        },
+        neon: {
+          orange: '#FF9800',
+          purple: '#9C27B0',
+          red: '#E53935',
+          green: '#4CAF50',
+          pink: '#FF4081',
+          yellow: '#FFFF00',
+        },
       },
     },
   },
